2b856086a5cb7123e4fbc971f8e0a76c18e77263
Keshav Verma <keshavverma4244@gmail.com>
2023-06-04 23:57:58 +0530

    sort 0s, 1s, 2s

---
+++ Day1 Array/Sort an array of 0's, 1's and 2's.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    /*
+        brute force:
+            sort the array
+                sort(nums.begin(),nums.end())
+
+            TC : O(N log(N))
+            SC : O(1)
+    */
+    /*
+        Counting    
+            1. count number of zero ,one ,two
+            2. fill array with zero -> one -> two
+
+                void sortColors(vector<int>& nums) {
+                    int nextZeros = 0, nextOnes = 0, nextTwos = 0;
+                    for(auto it : nums){
+                        if(it == 0) nextZeros++;
+                        else if(it == 1) nextOnes++;
+                        else nextTwos++;
+                    }
+                    int k = 0;
+                    for(int i=0; i<nextZeros; i++) nums[k++] = 0;
+                    for(int i=0; i<nextOnes; i++) nums[k++] = 1;
+                    for(int i=0; i<nextTwos; i++) nums[k++] = 2;
+                }
+            TC : O(N) + O(N)
+            SC : O(1)
+    */
+    /*
+        Pointers
+            _   _   _   _   _   _   _   _   _   _   _   _   _   _   
+            0           low             mid-1 mid            high high+1        n-1
+
+            0-low-1 => all 0
+            low - mid-1 => all 1
+            high+1 - n-1 => all 2
+
+            mid - high => solve
+
+            1. make 3 pointer low,mid,high  // low pointing to 0, mind to 1, high to 2
+            2. start from low = 0, mid = 0, high = n-1
+            3. if(nums[mid] == 0)
+                    swap(nums[low],nums[mid])
+                    low++
+                    mid++
+               if(nums[mid] == 1)
+                    mid++
+               if(nums[mid] == 2)
+                    swap(nums[mid],nums[high])
+                    high--;
+
+    */
+    void sortColors(vector<int>& nums) {
+        int low = 0, mid = 0, high = nums.size()-1;
+        while(high >= mid){
+            if(nums[mid] == 0){
+                swap(nums[mid],nums[low]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1)
+                mid++;
+            else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
+    }
+};
