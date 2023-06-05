dd0b4313d7e8a06a4b7d332c51b787b5458ca8b7
Keshav Verma <keshavverma4244@gmail.com>
2023-06-06 01:09:20 +0530

    merge ovelapping subintervals

---
+++ Day2 Array2/Merge Overlapping Subintervals.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    /*
+        Brute force :
+            -> first sort intervals;
+            -> Now traverse to all the elements in intervals & maintain a 
+            -> maintain 
+    */
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+    }
+};
