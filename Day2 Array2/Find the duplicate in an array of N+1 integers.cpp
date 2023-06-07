f0cecafadf8abe0bbff9eec2fd47c3ab46f236da
Keshav Verma <keshavverma4244@gmail.com>
2023-06-07 21:25:47 +0530

    find duplicate in an array

---
+++ Day2 Array2/Find the duplicate in an array of N+1 integers.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    // ** 7 approaches are presented here **
+    /*
+        1. Sort 
+            int findDuplicate(vector<int>& nums) {
+                sort(nums.begin(),nums.end());
+                for(int i=0;i<nums.size()-1;i++){
+                    if(nums[i] == nums[i+1])
+                        return nums[i];
+                }
+                return -1;
+            }
+
+            TC : O(n log(n))
+            SC : O(1)
+        2. Set
+            int findDuplicate(vector<int>& nums) {
+                unordered_set<int> us;
+                for(int i=0;i<nums.size();i++){
+                    int size = us.size();
+                    us.insert(nums[i]);
+                    if(us.size() == size)
+                        return nums[i];
+                }
+                return -1;
+            }
+
+            TC : O(n)
+            SC : O(n)
+            
+        3.Negative Marking
+
+
+        
+    */
+
+    
+    int findDuplicate(vector<int>& nums) {
+        
+    }
+
+};
