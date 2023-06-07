fb6799e7f6ba18a0e0e06c98fcb47ffa5b83e4c9
Keshav Verma <keshavverma4244@gmail.com>
2023-06-07 16:04:38 +0530

    merger overlapping

--- Day2 Array2/Merge Overlapping Subintervals.cpp
+++ Day2 Array2/Merge Overlapping Subintervals.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
     /*
-        Brute force :
+        Sorting :
             -> first sort intervals;
-            -> Now traverse to all the elements in intervals & maintain a 
-            -> maintain 
+            -> Now traverse to all the elements in intervals & compare it with the prev answer and store it
+            
+            TC :   O( n log(n))
+            SC :   O(n x n)
     */
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> ans;
+        sort(intervals.begin(),intervals.end());
+        ans.push_back(intervals[0]);
+        int ind = 0;
+        for(int i=1;i<intervals.size();i++){
+            if(intervals[i][0] <= ans[ind][1])
+                ans[ind][1] = max(intervals[i][1],ans[ind][1]);
+            else{
+                ans.push_back(intervals[i]);
+                ind++;
+            }
+        }
+        return ans;
     }
 };
